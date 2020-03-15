9d904afdc38cfefa74e13e55746bff7009601b6b
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2020-03-15 18:45:19 +0100

    aggiunta lezione 2

---
+++ lezione2/lez2_es1.c
@@ -0,0 +1,31 @@
+/*
+ comments
+ / plus *            init of the comment
+  untils * plus /    end of comment
+*/
+
+/* C program structure */
+
+/*
+   a program if a function with the
+   fixed name "main".
+   It return an integer (int).
+   The body of the program is contained 
+   between { }.
+   Parentheses () may contains the argument 
+   passed to the program (see forward)
+*/
+
+#include <stdio.h>
+
+int
+main() {
+  printf("ciao mona\n");
+  return 0;
+  /* return 0 = no error */
+}
+
+/*
+  to compile on a UNIX os.
+  > gcc example1.c -o example1
+*/
