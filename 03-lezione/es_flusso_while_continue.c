f6304a874cced948514e82cf269696bb4dfcb61f
tonegas <tonegas@gmail.com>
2021-03-15 22:25:00 +0100

    Lezione 3

---
+++ 03-lezione/es_flusso_while_continue.c
@@ -0,0 +1,7 @@
+/*
+ * Scrivere una funziona che prende un carattere char 
+ * e ritorna il numero massimo di ricorrenze consegutive di 1.
+ * es 13 -> 1101 -> 2
+ * Successivamente chiamare la funzione con 123 e 432
+ */
+ 
