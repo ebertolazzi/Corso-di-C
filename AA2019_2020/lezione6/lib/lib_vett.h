1db1eb9e6cd5d7c4774385fe09754ae717d06564
tonegas <tonegas@gmail.com>
2021-03-23 14:22:24 +0100

    riorganizzazione

--- AA2019_2020/lezione6/lib/lib_vett.h
+++
@@ -1,9 +0,0 @@
-
-// Questa funzione stampa il vettore
-void stampa_vettore(const int vett[], int size);
-
-// Questa funzione inserisce un elemento nel vettore 'vett'
-// in posizione 'ind' e ne incrementa il valore
-// ritorna 1 se viene letto il carattere 'f'
-// altrimenti torna 0
-int inserisci_elemento(int *vett, int *ind);
